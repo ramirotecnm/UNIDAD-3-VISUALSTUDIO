ad733e541898b848145d3c7254caad9640bf848d
ramirotecnm <ramiromartinezvelazques@gmail.com>
2024-11-16 18:06:56 -0600

    Add files via upload

---
+++ practica 13/practica 13/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, aum, nsue;
+            sueldo = Double.Parse(textBox1.Text);
+            if (sueldo < 400000.00) {
+                aum = sueldo * 0.15;
+                nsue = sueldo + aum;
+                textBox2.Text = nsue.ToString();
+            }
+        }
+    }
+}
---
+++ practica 15/practica 15/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, nsue;
+            sueldo = double.Parse(textBox1.Text);
+
+            if (sueldo < 400000) nsue = sueldo * 1.15;
+            else nsue = sueldo * 1.08;
+            textBox2.Text = nsue.ToString();
+
+        }
+    }
+}
